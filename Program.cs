e090e601b6c9b80e2784478557e142fc8e65423b
Linvega <ebeb.nado@yandex.ru>
2023-05-17 22:31:25 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab8
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Title = "Работа №8 Вариант 9 ФКТ 2 курс группа 219 Каргапольцев Кирилл";
+
+            double x, toch;          //инициализация очень нужных переменных
+            int num, a_prov = 0;
+            do   //цикл с постусловием для проверки введённого числа Х
+            {
+                Console.Clear();
+                Console.WriteLine("Объект 1 \nВычислние ряда с заданной точностью\narctg x = x - x^3/3 + x^5/5 - x^7/7 + ... \nЭлемент ряда = (-1)^n * x^(2*n+1) / (2*n + 1)"); //вступление
+                Console.WriteLine("----------------------------------------------------");
+                if (a_prov == 1)//проверка на наличие ошибок(чтобы сообщение не показывалось при первом проходе)
+                {
+                    Console.WriteLine("Ошибка диапазона числа х!");   //сообщение начнёт выводиться только 
+                }                                                     //после второго прогона этого цикла
+                Console.Write("Введите значение (|x| < 1) x: "); 
+                x = Convert.ToDouble(Console.ReadLine()); //ввод числа Х
+                a_prov++; //переменная для проверки ошибки введённых данных
+            }while (Math.Abs(x) > 1);  //проверяем выход за диапазон
+            Console.Write("Введите кол-во элементов ряда: ");
+            num = Convert.ToInt32(Console.ReadLine());       //ввод кол-ва элементов ряда
+            Console.WriteLine("Значение элементов ряда");
+            Console.WriteLine("----------------------------------------------------");
+            Project obj_1 = new Project(x,num); //создание объекта 1 с первым конструктором
+            Console.Write(obj_1.mass());  //вывод все элементов
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine("MyAtan("+x+") :"+obj_1.result()); //вывод значения моей функции
+            Console.WriteLine("Mach.Atan("+x+") :"+ Math.Atan(x)); //вывод значения мат. функции
+            Console.WriteLine("Погрешность: "+(Math.Atan(x) - obj_1.result())); //вычисление погрешности
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine("Объект 2");
+            Console.Write("Введите точность: ");
+            toch = Convert.ToDouble(Console.ReadLine());  //ввод погрешности для второй части вычислений
+            Console.WriteLine("----------------------------------------------------");
+            Project obj_2 = new Project(toch,x); //создание объекта 2 с перегруженным конструктором
+            Console.Write(obj_2.mass());  //вывод массива необходимого кол-ва элементов
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine("MyAtan(" + x + ") :" + obj_2.result()); //вывод значения моей функции с учётом заданной точности
+            Console.WriteLine("Mach.Atan(" + x + ") :" + Math.Atan(x)); //вывод значения мат. функции
+            Console.WriteLine("Погрешность2: " + (Math.Atan(x) - obj_2.result()));  //вычисление погрешности
+
+            Console.WriteLine("\nНажмите любую клавишу чтобы выйти..."); //завершение программы
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Project.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab8
+{
+    abstract class Project_osn   //создание базового абстрактного класса
+    {
+        public double element(double x_e, int num_e)  //инициализация метода для вычисления значения заданного элемента
+        {
+            double elem = Math.Pow(-1,num_e) * Math.Pow(x_e,2*num_e+1) / (2*num_e+1); //формула для расчёта 1 элемента
+            return elem;  //возвращение значения элемента
+        }
+    }
+
+    class  Project : Project_osn     //создание дочернего класса
+    {
+        private double znach, x_trabl;   //инициализация переменных
+        private double[] mass_znach;     //инициализация массива
+        private int a = 0;
+        public Project(double x_c, int num_c)     //создание конструктора 
+        {
+            x_trabl = x_c;    //считываем значение Х
+            a = num_c;        //считываем кол-во элементов для массива
+            mass_znach = new double[a];  //указываем кол-во элементов массива
+            for (int i = 0; i < a ; i++)   //заполняем массив значения и ищем общее значение
+            {
+                mass_znach[i] = element(x_c, i);
+                znach += element(x_c,i);
+            }
+        }
+        public Project(double t, double x_toch)    //создание перегруженного конструктора
+        {
+            x_trabl = x_toch;    //считываем Х
+            while (Math.Abs(element(x_toch,a)) > t) //ищем необходимое кол-во элементов удовлетворяющее точности
+            {
+                a++;
+            }
+            mass_znach = new double[a]; //задаём размер массива
+            for (int i = 0; i < a; i++) //заполняем массив и ищем общее значение функции
+            {
+                mass_znach[i] = element(x_toch,i);
+                znach += element(x_toch, i);
+            }
+        }
+
+        public string mass()    //метод для вывода массива значений
+        {
+            string ret = ""; //создание строковой переменной
+            for (int i = 0; i < a; i++)  //цикл для заполнения переменной значениями массива
+            {
+                int need = 25;               //сдесь начинается сложная и корявая система отступов
+                if (element(x_trabl, 0) > 0) //проверяем минусовой первый элемент или нет
+                {
+                    if ((i + 1) % 2 != 0) { need++; }    //сдесь
+                }                                        //настраивается
+                else                                     //выравнивание
+                {                                        //в зависимости
+                    if ((i + 1) % 2 == 0) { need++; }    //от знака
+                }                                        //и размера
+                if ((i + 1) / 10 < 1) { need++; }        //числа  
+                if ((i + 1) / 100 < 1) { need++; }       //работает до 3-х значных чисел включительно
+                ret += (i + 1) + (new String(' ', need)) + mass_znach[i] + "\n"; //постепенно заполняем переменную значениями
+            }
+            return ret; //возвращаем гиганскую строку данных
+        }
+
+        public double result()  //метод для вывода результата расчётов функции
+        {
+            return znach;
+        }
+    }
+}
